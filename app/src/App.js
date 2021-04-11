dd67d0df671658d2c319fa0292d11b9cee078aa1
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-04-11 20:31:54 +0300

    Initial commit

---
+++ app/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import Login from './Login';
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Register from './Register';
+import DriveReq from './DriveReq.js';
+
+
+
+
+
+function App(p) {
+  return (
+    <BrowserRouter>
+    
+    <Link to="/Register">הרשמה</Link>
+    <span> | </span>
+    
+    <Switch>
+
+      <Route path="/" exact component={Login}/>
+      <Route path="/Register" component={Register}/>
+      <Route path="/DriveReq" component={DriveReq}/>
+
+      <Route path="*" />
+    </Switch>
+
+
+      
+    </BrowserRouter>
+   
+  );
+}
+
+export default App;
