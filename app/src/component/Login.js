c9c4c7ee124346aef85da0f74693fdfd16150bdd
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-04-20 18:45:02 +0300

    Update Login.js

--- app/src/component/Login.js
+++ app/src/component/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
 import '../style/Login.css';
-import { Link } from 'react-router-dom';
+import { Link, } from 'react-router-dom';
 
 
 
@@ -14,27 +14,34 @@ function Login(){
 
 
     return(
-    <div class="container Login text-*-center">
-        <h1 class="text-center">התחברות</h1>
+    <div class="container-fluid  log-bg  ">
         <div class="row">
-            <div class="col-lg-4 col-xs-12">
-                <form>
-                    <label for="email">:מייל</label>
-                        <br/>
-                        <input placeholder="הכנס מייל" class="form-control"></input>
-                            <label for="pwd">:סיסמה</label>
-                            <br/>
+        <div class="col-md-4 col-sm-4 col-xs-12 "></div>
+            <div class="col-md-4 col-sm-4 col-xs-12 ">
+                <form class="from-container text-light">
+                    <h1 class="text-center">כניסה</h1>
+                    <div class="form-group">
+                        <h5 class=" text-right">שם משתמש</h5>
+                            <input placeholder="Example@Example.com" class="form-control"></input>
+                    </div>
+                    <div class="form-group">
+                        <h5 class=" text-right">סיסמה</h5>
                             <input type="password" placeholder="הכנס סיסמה" class="form-control" ></input>
-                            <Button type="button" class="btn btn-primary" >התחברות</Button>
-        
-                    <Link to="/Register">
-                        <Button type="button" class="btn btn-primary">הרשמה</Button>
-                    </Link>
+                    </div>
+                    <div>
+                        <Link to ="DriveReq/">
+                            <Button type="button" class="btn " >התחברות</Button>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to="/Register">
+                            <Button type="button" class="btn btn-dark btn-block">הרשמה</Button>
+                        </Link>
+                    </div>
                     </form>
+                </div>
             </div>
         </div>
-    </div>
-        
     );
 }
 
