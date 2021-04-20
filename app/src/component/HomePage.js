fe431c898cfd4ca5529a00c39196ff49bc771f85
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-04-20 18:44:57 +0300

    Create HomePage.js

---
+++ app/src/component/HomePage.js
@@ -0,0 +1,14 @@
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ function Home_page(){
+        return(
+            <div>
+                <h1>aa</h1>
+            </div>
+        );
+    }
+ 
+    return Home_page;
