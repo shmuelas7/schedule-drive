b5e82dadf482cbada589f73d0c497b355035a027
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-05-03 13:31:30 +0300

    Update Register.js

--- app/src/component/Register.js
+++ app/src/component/Register.js
@@ -13,36 +13,36 @@ import  '../style/Register.css';
                 <div class="col-md-4 col-sm-4 col-xs-12 "></div>
                 <div class="col-md-4 col-sm-4 col-xs-12 ">
                 </div>
-                <div class="col-md-4 col-sm-4 col-xs-12 ">
+                <div class="col-md-4 col-sm-12 col-xs-12 ">
                     <form class="from-container text-light" >
                         <h1 class="text-center">טופס הרשמה</h1>
                         <div class="from-group">
                             <h5 class=" text-right">שם פרטי</h5>
-                            <input placeholder="שם פרטי*" class="form-control" required></input>
+                            <input placeholder="שם פרטי*" class="form-control text-right" required></input>
                         </div>
                         <div>
                             <h5 class=" text-right" >שם משפחה</h5>
-                            <input placeholder="שם משפחה*" class="form-control" required ></input>
+                            <input placeholder="שם משפחה*" class="form-control text-right" required ></input>
                         </div>
                         <div>
                             <h5 class=" text-right">מייל</h5>
-                            <input placeholder="מייל*" class="form-control" required ></input>
+                            <input placeholder="מייל*" class="form-control text-right" required ></input>
                         </div>
                         <div>
                             <h5 h5 class=" text-right">טלפון</h5>
-                            <input placeholder="מספר נייד*" class="form-control" required ></input>
+                            <input placeholder="מספר נייד*" class="form-control text-right" required ></input>
                         </div>
                         <div>
                             <h5 class=" text-right">  סיסמה</h5>
-                            <input type="password" placeholder="סיסמה*" class="form-control" required id="pwd" name="password"></input>
+                            <input type="password" placeholder="סיסמה*" class="form-control text-right" required id="pwd" name="password"></input>
                         </div>
                         <div>
                             <h5 class=" text-right">אימות סיסמה</h5>
-                            <input type="password" placeholder="אישור סיסמה*" class="form-control" required id="pwd" name="password"></input>
+                            <input type="password" placeholder="אישור סיסמה*" class="form-control text-right" required id="pwd" name="password"></input>
                         </div>
                         <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
-                        <h5 class="form-check-label" for="inlineRadio1">מסיעה</h5>
+                        <h5 class="form-check-label " for="inlineRadio1">מסיע</h5>
                         </div>
                         <div class="form-check form-check-inline ">
                         <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
