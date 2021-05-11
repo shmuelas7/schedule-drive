cff1b181908f14817936db880b32bbad07b99075
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-05-11 15:16:34 +0300

    Create Nav.js

---
+++ app/src/component/Nav.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+import Icon from "./icon_profile";
+import Car from "./Icon_car"
+import Wheel from "./Wheel"
+
+
+function Nav(){
+
+    return(
+        <nav>
+        <div class="row">
+            <div class="col-md-4 col-sm-12 col-xs-12 text-center">
+               <Link to="profile" >
+                   <Icon/>
+                   <label class="text-center"> אזור אישי</label>
+               </Link> 
+           </div>
+
+           <div class="col-md-4 col-sm-12 col-xs-12 text-center ">
+           <Link to="profile" >
+                   <Wheel/>
+               <label>נסיעות שבוצעו</label>
+            </Link>
+           </div>
+
+           <div class="col-md-4 col-sm-12 col-xs-12 text-center ">
+           <Link to="profile" >
+           <Car/>
+               <label>נסיעות עתידיות</label>
+            </Link>
+           </div>
+
+       </div>
+       </nav>
+    );
+}
+
+export default Nav;
