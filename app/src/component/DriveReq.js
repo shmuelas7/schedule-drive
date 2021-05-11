a998329020f5ed35bd9173a9c711c2b61f184ea5
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-05-11 15:16:21 +0300

    Update DriveReq.js

--- app/src/component/DriveReq.js
+++ app/src/component/DriveReq.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+import Nav from "./Nav";
 
 
 
@@ -8,20 +10,21 @@ function Req(){
 
     return(
         <div class="container-fluid Req-bg">
+            <Nav/>
+ 
             <div class="row">
                 <div class="col-md-4 col-sm-4 col-xs-12 "></div>
+               
                     <div class="col-md-4 col-sm-4 col-xs-12">
-                        <input placeholder="תאריך יציאה" class="form-control" required></input>
-                        <input placeholder="מוצא" class="form-control" required ></input>
-                        <input placeholder="יעד" class="form-control" required ></input>
-                        <input placeholder="שעה" class="form-control" required ></input>
-
+                        <h1 class="text-center">  הכנס זמני נסיעה ויעד</h1>
+                        <input  type="date" placeholder="תאריך יציאה"class="form-control " required></input>
+                        <input placeholder="מוצא" class="form-control text-right" required ></input>
+                        <input placeholder="יעד" class="form-control text-right" required ></input>
+                        <input  type="time" placeholder="שעה" class="form-control " required ></input>
 
                     </div>
                 </div>
-            
-
-            </div>
+    </div>
     );
 }
 
