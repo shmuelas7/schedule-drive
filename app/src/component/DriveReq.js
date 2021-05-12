4fcbf42b359211de8415565b6add64459e8fafc8
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-05-12 11:42:05 +0300

    Update DriveReq.js

--- app/src/component/DriveReq.js
+++ app/src/component/DriveReq.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
+import '../style/DriveReq.css';
 import Nav from "./Nav";
 
 
@@ -11,17 +10,17 @@ function Req(){
     return(
         <div class="container-fluid Req-bg">
             <Nav/>
- 
             <div class="row">
+                <div class="col-md-4 col-sm-4 col-xs-12 "></div>
                 <div class="col-md-4 col-sm-4 col-xs-12 "></div>
                
                     <div class="col-md-4 col-sm-4 col-xs-12">
-                        <h1 class="text-center">  הכנס זמני נסיעה ויעד</h1>
-                        <input  type="date" placeholder="תאריך יציאה"class="form-control " required></input>
-                        <input placeholder="מוצא" class="form-control text-right" required ></input>
-                        <input placeholder="יעד" class="form-control text-right" required ></input>
-                        <input  type="time" placeholder="שעה" class="form-control " required ></input>
-
+                        <h1 class="text-center h1-d">  הכנס זמני נסיעה ויעד</h1>
+                        <input  type="date" placeholder="תאריך יציאה"class="form-control  input-d" required></input>
+                        <input placeholder="מוצא" class="form-control text-right input-d" required ></input>
+                        <input placeholder="יעד" class="form-control text-right input-d" required ></input>
+                        <input  type="time" placeholder="שעה" class="form-control input-d " required ></input>
+                        <Button type="submit" variant="dark"  block className="log-btn">שלח</Button>
                     </div>
                 </div>
     </div>
