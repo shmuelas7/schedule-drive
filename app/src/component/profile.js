f374d693a083edb0241e2557e79ee2039628985b
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-05-12 11:42:14 +0300

    Update profile.js

---
+++ app/src/component/profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Nav from "./Nav";
+
+function Profile(){
+
+    return(
+       
+        <div class="container-fluid">
+             <Nav/>
+             <h1>hllo</h1>
+        </div>
+    );
+}
+export default Profile;
