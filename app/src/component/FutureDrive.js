ceabec727d052ac4ec9ed6761ab750ed8420c92e
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-05-22 23:23:53 +0300

    Create FutureDrive.js

---
+++ app/src/component/FutureDrive.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Nav from "./Nav";
+import Table from 'react-bootstrap/Table'
+  
+
+function FutureDrive(){
+    return(
+        <div class="container-fluid Req-bg" >
+
+            <Nav/>
+            <Table striped bordered hover variant="dark">
+                <thead class="text-right">
+                    <tr>
+                    <th>אשר נסיעה</th>
+                    <th>מספר טלפון</th>
+                    <th>יעד</th>
+                    <th>מוצא</th>
+                    <th>שעה</th>
+                    <th>תאריך</th>
+                    <th>שם המבקש</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </Table>
+        </div>
+
+
+    )
+}
+
+export default FutureDrive;
