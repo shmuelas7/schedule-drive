00464919e523d49f2bc841e7b3aaa0b17ccf107c
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-05-22 23:23:50 +0300

    Update Driver.js

--- app/src/component/Driver.js
+++ app/src/component/Driver.js
@@ -10,40 +10,15 @@ function Driver(){
             <Nav/>
             <Table striped bordered hover variant="dark">
                 <thead>
-                    <tr>
-                    <th>שם המבקש</th>
-                    <th>תאריך</th>
-                    <th>מוצא</th>
+                <th>אשר נסיעה</th>
+                    <th>מספר טלפון</th>
                     <th>יעד</th>
+                    <th>מוצא</th>
                     <th>שעה</th>
-                    <th>מספר פלאפון</th>
-                    </tr>
+                    <th>תאריך</th>
+                    <th>שם המבקש</th>
                 </thead>
                 <tbody>
-                    <tr>
-                    <td>משה לוי</td>
-                    <td>25.5</td>
-                    <td>אריאל</td>
-                    <td>פתח תקווה</td>
-                    <td>10:30</td>
-                    <td>053616314</td>
-                    </tr>
-                    <tr>
-                    <td>נועם גולן</td>
-                    <td>1.6</td>
-                    <td>הרצליה</td>
-                    <td>אשדוד</td>
-                    <td>15:15</td>
-                    <td>052154356</td>
-                    </tr>
-                    <tr>
-                    <td>עמית ששון</td>
-                    <td>19.5</td>
-                    <td>חיפה</td>
-                    <td>תל אביב</td>
-                    <td>7:00</td>
-                    <td>050346837</td>
-                    </tr>
                 </tbody>
             </Table>
         </div>
