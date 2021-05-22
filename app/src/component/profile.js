35407a72861b3c25a1b61e64519f4ed81a37aa81
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-05-22 23:24:06 +0300

    Update profile.js

--- app/src/component/profile.js
+++ app/src/component/profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 import Nav from "./Nav";
 
 function Profile(){
@@ -7,8 +7,32 @@ function Profile(){
     return(
        
         <div class="container-fluid">
-             <Nav/>
-             <h1>hllo</h1>
+            <Nav/>
+            <div class="row">
+            <div class="col-md-4 col-sm-4 col-xs-12 "></div>
+            <div class="col-md-4 col-sm-4 col-xs-12 ">
+            <form >
+                        <h1 class="text-center">עידכון פרטיים אישיים </h1>
+
+                        <div>
+                            <h5 h5 class=" text-right"> עידכון מספר טלפון</h5>
+                            <input placeholder="מספר נייד*" class="form-control text-right" ></input>
+                        </div>
+                        <div>
+                            <h5 class=" text-right">  שינוי סיסמה</h5>
+                            <input type="password" placeholder="הכנס סיסמה ישנה" class="form-control text-right" ></input>
+                            <input type="password" placeholder="הכנס סיסמה חדשה" class="form-control text-right" ></input>
+                        </div>
+                        <h5 class=" text-right">שינוי תמונת פרופיל</h5>
+                        <div class="custom-file">
+                             <input type="file" class="custom-file-input" id="customFile"/>
+                            <label class="custom-file-label " for="customFile">בחר תמונה</label>
+                        </div>
+
+                        <Button type="submit" variant="dark"  block  className="log-btn">שלח</Button>              
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
