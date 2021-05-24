d14d1d2f7338ddc86c3077617aa2aba931653273
asih94 <asafhager94@gmail.com>
2021-05-24 13:04:34 +0300

    Update DriveReq.js

--- app/src/component/DriveReq.js
+++ app/src/component/DriveReq.js
@@ -15,13 +15,23 @@ function Req(){
                 <div class="col-md-4 col-sm-4 col-xs-12 "></div>
                
                     <div class="col-md-4 col-sm-4 col-xs-12">
-                        <h1 class="text-center h1-d">  הכנס זמני נסיעה ויעד</h1>
+                        <h1 class="text-center h1-d">בקשת נסיעה</h1>
                         <input  type="date" placeholder="תאריך יציאה"class="form-control  input-d" required></input>
                         <input placeholder="מוצא" class="form-control text-right input-d" required ></input>
                         <input placeholder="יעד" class="form-control text-right input-d" required ></input>
                         <input  type="time" placeholder="שעה" class="form-control input-d " required ></input>
                         <Button type="submit" variant="dark"  block className="log-btn">שלח</Button>
+
+                        <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+                        <h5 class="form-check-label " for="inlineRadio1">מלווה</h5>
+                        </div>
+                        <div class="form-check form-check-inline ">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                        <h5 class="form-check-label" for="inlineRadio2">כיסא גלגלים</h5>
+                        </div>
                     </div>
+                   
                 </div>
     </div>
     );
