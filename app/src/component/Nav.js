9703bada63debf08f6e3928a6fab725fb73c4b76
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-08-12 20:22:05 +0300

    Update Nav.js

--- app/src/component/Nav.js
+++ app/src/component/Nav.js
@@ -1,38 +1,58 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
-import Icon from "./icon_profile";
-import Car from "./Icon_car"
-import Wheel from "./Wheel"
+import Icon from "../icon/icon_profile";
+import Car from "../icon/Icon_car";
+import Wheel from "../icon/Wheel";
+import caRQ from "../icon/icon_caRQ";
+import '../style/nav.css';
+import firebase from 'firebase';
 
 
 
 function Nav(){
 
+    function signout(){
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+      }).catch((error) => {
+        // An error happened.
+      });
+    }
+
     return(
         <nav>
+            <div class="text-center logo">
+            </div>
         <div class="row">
-            <div class="col-md-4 col-sm-12 col-xs-12 text-center">
+
+            <div class="col-md-3 col-sm-12 col-xs-12 text-center">
                <Link to="Profile" >
                    <Icon/>
                    <label class="text-center"> אזור אישי</label>
                </Link> 
            </div>
 
-           <div class="col-md-4 col-sm-12 col-xs-12 text-center ">
+           <div class="col-md-3 col-sm-12 col-xs-12 text-center ">
            <Link to="PreviousDrive" >
                    <Wheel/>
                <label>נסיעות שבוצעו</label>
             </Link>
            </div>
 
-           <div class="col-md-4 col-sm-12 col-xs-12 text-center ">
+           <div class="col-md-3 col-sm-12 col-xs-12 text-center ">
            <Link to="FutureDrive" >
-           <Car/>
+            <Car/>
                <label>נסיעות עתידיות</label>
             </Link>
            </div>
 
+           <div class="col-md-3 col-sm-12 col-xs-12 text-center ">
+           <Link to="DriveReq" >
+                <caRQ/>
+               <label>בקשת נסיעה </label>
+            </Link>
+           </div>
        </div>
        </nav>
     );
