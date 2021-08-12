49d7a33884eb043026e7af0c436cc699ca1a9810
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-08-12 20:21:38 +0300

    Update App.js

--- app/src/component/App.js
+++ app/src/component/App.js
@@ -1,12 +1,14 @@
 import '../style/App.css';
 import Login from './Login';
-import { BrowserRouter,  Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Register from './Register';
 import DriveReq from './DriveReq';
 import Profile from './Profile';
 import Driver from './Driver';
 import FutureDrive from './FutureDrive'
 import PreviousDrive from './PreviousDrive'
+import { AuthProvider } from "../contexts/AuthContext"
+import PrivateRoute from "./PrivateRoute"
 
 
 
@@ -15,26 +17,27 @@ import PreviousDrive from './PreviousDrive'
 
 
 function App(p) {
+
   return (
-  
-      <BrowserRouter>
+      
+      <Router>
+
+    <AuthProvider>
         <Switch>
+          
           <Route path="/" exact component={Login}/>
+          <PrivateRoute path="/" exact component={Driver} />
           <Route path="/Register" component={Register}/>
           <Route path="/DriveReq" component={DriveReq}/>
           <Route path="/Profile" component={Profile}/>
-          <Route path="/Driver" component={Driver}/>
           <Route path="/FutureDrive" component={FutureDrive}/>
           <Route path="/PreviousDrive" component={PreviousDrive}/>
-
           <Route path="*" />
+          
         </Switch>
-
-      </BrowserRouter>
-
-    
-
-   
+        </AuthProvider>
+      
+      </Router>   
   );
 }
 
