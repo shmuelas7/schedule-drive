d81f79c196124f6ff50cd6e43983d878b037e514
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-08-20 11:06:52 +0300

    Update Driver.js

--- app/src/component/Driver.js
+++ app/src/component/Driver.js
@@ -1,33 +1,16 @@
-import {React, useState} from 'react';
+import {React} from 'react';
 import Nav from "./Nav";
 import Table from 'react-bootstrap/Table'
-import { useAuth } from "../contexts/AuthContext"
-import { Link, useHistory } from "react-router-dom"
-import { Alert } from "react-bootstrap"
+
 
 
   
 
 function Driver(){
 
-    const [error, setError] = useState("")
-    const { currentUser, logout } = useAuth()
-    const history = useHistory()
-  
-    async function handleLogout() {
-      setError("")
-  
-      try {
-        await logout()
-        history.push("/login")
-      } catch {
-        setError("Failed to log out")
-      }
-    }
+
     return(
         <div class="container-fluid Req-bg">
-        {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
             <Nav/>
             <Table striped bordered hover variant="dark">
                 <thead>
