ca7207ad6243ca17f01fffbd92e0648ec7e4bac4
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-08-24 17:00:06 +0300

    Update Footer.js

--- app/src/component/Footer.js
+++ app/src/component/Footer.js
@@ -1,51 +1,38 @@
 import {React} from 'react';
 import Button  from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "../style/white logo.png"
+import "../style/footer.css"
+
 function Footer(){
 
     return(
 
     <footer className="bg-dark text-center text-white" dir="rtl">
-    
-    <div className="container p-4 pb-0">
-    <strong className="text-right ">ליצירת קשר עם העמותה</strong>
+        <div className="container p-4 pb-0">
+            <strong className="text-center ">ליצירת קשר עם העמותה</strong>
+        
+            <form action="">
+                <div className="row d-flex justify-content-center">
+                    <div className="col-md-5 col-12">
+                        <div className="form-outline form-white mb-4">
+                        <textarea placeholder="כתוב כאן" className="form-control text-right"></textarea>
+                        </div>
+                    </div>
 
-    
-        <form action="">
-
-            <div className="row d-flex justify-content-center">
-            <div className="col-auto">
-                <p className="pt-2 ">
-                </p>
-            </div>
-            <div className="col-md-5 col-12">
-
-                <div className="form-outline form-white mb-4">
-                <textarea placeholder="כתוב כאן" className="form-control text-right"></textarea>
-
+                    <div className="col-auto">
+                        <Button type="submit" className="btn btn-outline-light mb-4">
+                        שלח
+                        </Button>
+                    </div>
                 </div>
-            </div>
-
-
-
-            <div className="col-auto">
-
-                <Button type="submit" className="btn btn-outline-light mb-4">
-                שלח
-                </Button>
-            </div>
-            
-            </div>
-    
-        </form>
-
-    
-    </div>
-
-    <div className="text-center p-3">
-        © האתר נבנה ע"י : 
-        <a className="text-white"> שמואל אשרוב ואסף הגר</a>
-    </div>
+            </form>
+        </div>
+        <div className="text-center p-3">
+            © האתר נבנה ע"י : 
+            <p className="text-white"> שמואל אשרוב ואסף הגר</p>
+            <img src={logo} alt="logo" className="flogo text-left"/>
+        </div>
 
     </footer>
   
