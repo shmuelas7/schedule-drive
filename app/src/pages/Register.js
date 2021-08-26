4965b4339505813679f992cfd27e0c18ab289b0b
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-08-26 19:40:13 +0300

    Update Register.js

--- app/src/pages/Register.js
+++ app/src/pages/Register.js
@@ -6,7 +6,7 @@ import { useState,useRef} from 'react';
 import { useAuth } from "../contexts/AuthContext"
 import {  useHistory } from "react-router-dom"
 import firebase from 'firebase';
-import logo from '../style/white logo.png'
+import logo from '../style/black logo.png'
 
 
 
@@ -23,6 +23,7 @@ import logo from '../style/white logo.png'
         const [lasttname,setlastname]=useState("")
         const [phone,setphone]=useState("")
         const [area,setarea]=useState("")
+        const [age,setage]=useState(0);
     
  
 
@@ -52,7 +53,8 @@ import logo from '../style/white logo.png'
       first_name:firstname,
       last_name:lasttname,
       phone_number:phone,
-      area:area
+      area:area,
+      age:age
     })
 
     }
@@ -71,56 +73,67 @@ import logo from '../style/white logo.png'
 
     return(
         <div className="container-fluid bg-primary">
-            
+
             <div className="row">
-                <div className="col-md-4 col-sm-4 col-xs-12 "></div>
-                <img src={logo} alt="logo" className="rg-logo"/>
-                <div className="col-md-4 col-sm-4 col-xs-12 ">
-                </div>
-                <div className="col-md-4 col-sm-12 col-xs-12" >
-                {error && <Alert variant="danger">{error}</Alert>}
-                <h1 className="text-center">טופס הרשמה</h1>
-                    <form className="center" id="user-data" onSubmit={handleSubmit}>
-                        
-                        <div className="from-group">
-                            <h5 className=" text-right">שם פרטי</h5>
-                            <input placeholder="שם פרטי*" className="form-control text-right" name="firstName" type="text" onChange={(e)=>setfirstname(e.target.value)}   required></input>
-                        </div>
-                        <div>
+                <div className="col-md-3 col-sm-12 col-xs-12 "></div>
+                
+                
+                <div className="col-md-6 col-sm-12 col-xs-12" >
+                    {error && <Alert variant="danger">{error}</Alert>}  
+                    <form className=" bg-light rounded  border border-dark mb-2  mt-2"  onSubmit={handleSubmit}>
+                    <img src={logo} alt="logo" className="rg-logo"/>
+                    <h1 className="text-center">טופס הרשמה</h1>
+                    
+                    <div className="row">
+                        <div className="from-gtoup col-md-6 col-sm-12">
                             <h5 className=" text-right" >שם משפחה</h5>
                             <input placeholder="שם משפחה*" className="form-control text-right" name="lastName" required onChange={(e)=>setlastname(e.target.value)} ></input>
-                        </div>
-                        <div>
+
                             <h5 className=" text-right">מייל</h5>
                             <input placeholder="מייל*" className="form-control text-right" required  ref={emailRef}></input>
+
+                            <h5 className=" text-right">גיל</h5>
+                            <input type="number" placeholder="גיל" className="form-control text-right" required  name="age" onChange={(e)=>setage(e.target.value)}></input>
+
+                            <h5 className=" text-right">אימות סיסמה</h5>
+                            <input type="password" placeholder="אישור סיסמה*" className="form-control text-right" required  name="password" ref={passwordConfirmRef} autoComplete="off"></input> 
+                    
                         </div>
-                        <div>
+                        <div className="from-group col-md-6 col-sm-12">
+                            <h5 className=" text-right">שם פרטי</h5>
+                            <input placeholder="שם פרטי*" className="form-control text-right" name="firstName" type="text" onChange={(e)=>setfirstname(e.target.value)}   required></input>
+                        
+
                             <h5  className=" text-right">טלפון</h5>
                             <input placeholder="מספר נייד*" className="form-control text-right" required onChange={(e)=>setphone(e.target.value)}></input>
-                        </div>
-                        <div>
+                            
                             <h5 className=" text-right" >אזור</h5>
                             <input placeholder="בחר אזור*" className="form-control text-right" required onChange={(e)=>setarea(e.target.value)} ></input>
-                        </div>
-                        <div>
+
                             <h5 className=" text-right">  סיסמה</h5>
                             <input type="password" placeholder="סיסמה*" className="form-control text-right" required  name="password" ref={passwordRef} autoComplete="off"></input>
-                        </div>
-                        <div>
-                            <h5 className=" text-right">אימות סיסמה</h5>
-                            <input type="password" placeholder="אישור סיסמה*" className="form-control text-right" required  name="password" ref={passwordConfirmRef} autoComplete="off"></input>
-                        </div>
-                        <h5 className=" text-right">הוספת תמונת פרופיל</h5>
+                            <h5 className=" text-right">הוספת תמונת פרופיל</h5>
                         <div className="custom-file">
                              <input type="file" className="custom-file-input" id="customFile"/>
                             <label className="custom-file-label ">בחר תמונה</label>
                         </div>
                       
-                        <Button type="submit" disabled={loading} variant="dark"  block  className="log-btn">שלח</Button>              
+                        </div>
+
+ 
+                        <Button type="submit" disabled={loading} variant="success"  block  className="log-btn mx-5 mb-2 btn-lg">שלח</Button>         
+                        
+                    </div>
                     </form>
                 </div>
+                <div className="col-3">
+
+                </div>
+                
             </div>
+           
         </div>
+        
     );
 }
 
