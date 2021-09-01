e74eff2802a6848be5d972d29d84b081130105ea
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-09-01 09:09:28 +0300

    Update Footer.js

--- app/src/component/Footer.js
+++ app/src/component/Footer.js
@@ -1,40 +1,37 @@
 import {React} from 'react';
-import Button  from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from "../style/white logo.png"
 import "../style/footer.css"
-
+import { Link } from 'react-router-dom';
 function Footer(){
 
     return(
 
-    <footer className="bg-dark text-center text-white footer"  dir="rtl">
-        <div className="container pb-1">
-            <strong className="text-center ">ליצירת קשר עם העמותה</strong>
-        
-            <form action="">
-                <div className="row d-flex justify-content-center">
-                    <div className="col-md-5 col-12">
-                        <div className="form-outline form-white mb-4">
-                        <textarea placeholder="כתוב כאן" className="form-control text-right"></textarea>
-                        </div>
-                    </div>
-
-                    <div className="col-auto">
-                        <Button type="submit" className="btn btn-outline-light mb-4">
-                        שלח
-                        </Button>
-                    </div>
+    <div className="container-fluid">
+        <footer className="bg-dark text-center text-white "  dir="rtl">
+            <div className="row">
+                <div className="col-6">
+                    <Link to="contact">
+                        <p className="text-center ">ליצירת קשר עם העמותה</p>
+                    </Link>
+                    <Link to="gift">
+                        <p className="text-center ">דף תרומות לעמותה</p>
+                    </Link>
                 </div>
-            </form>
-        </div>
-        <div className="text-center">
-            © האתר נבנה ע"י : 
-            <p className="text-white"> שמואל אשרוב ואסף הגר</p>
-            <img src={logo} alt="logo" className="flogo text-left"/>
-        </div>
+                <div className="col-6"></div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <p className="text-white text-center"> האתר נבנה ע"י שמואל אשרוב ואסף הגר ©</p>
+                </div>
+                    
+                
+            </div>
+                
+    
+
 
     </footer>
+    </div>
   
     );
 }
