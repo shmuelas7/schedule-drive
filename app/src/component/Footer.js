0663af00059d641cf2a6dbaefd3bba81d2bc9310
asih94 <asafhager94@gmail.com>
2021-09-05 13:33:26 +0300

    Update Footer.js

--- app/src/component/Footer.js
+++ app/src/component/Footer.js
@@ -7,21 +7,21 @@ function Footer(){
     return(
 
     <div className="container-fluid">
-        <footer className="bg-dark text-center text-white "  dir="rtl">
+        <footer className="bg-dark text-center text-white"  dir="rtl">
             <div className="row">
-                <div className="col-6">
+                <div className="col-12">
                     <Link to="contact">
-                        <p className="text-center ">ליצירת קשר עם העמותה</p>
+                        <p className="text-center text-info"><b>ליצירת קשר עם העמותה</b></p>
                     </Link>
                     <Link to="gift">
-                        <p className="text-center ">דף תרומות לעמותה</p>
+                        <p className="text-center text-success"><b>דף תרומות לעמותה</b></p>
                     </Link>
                 </div>
                 <div className="col-6"></div>
             </div>
             <div className="row">
                 <div className="col-12">
-                    <p className="text-white text-center"> האתר נבנה ע"י שמואל אשרוב ואסף הגר ©</p>
+                    <p className="text-white text-center"><b>האתר נבנה ע"י שמואל אשרוב ואסף הגר ©</b></p>
                 </div>
                     
                 
