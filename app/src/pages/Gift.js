555c6b64b4a4926ecbd3ba85cda7b464879263fb
asih94 <asafhager94@gmail.com>
2021-09-05 13:33:37 +0300

    Update Gift.js

---
+++ app/src/pages/Gift.js
@@ -0,0 +1,25 @@
+import {React} from 'react';
+import Nav from "../component/Nav";
+
+
+function Gift(){
+
+
+    return(
+        <div className="container-fluid">
+            <Nav/>
+            
+            <div className="bg-warning">
+            <h1 className="text-center">תרומות לעמותה</h1>
+
+       
+                <tbody>
+                </tbody>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default Gift;
