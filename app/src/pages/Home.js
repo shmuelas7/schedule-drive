bc67e24ee9dee5e50f0d5193d63a1293fba9efcd
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-09-13 00:50:17 +0300

    Update Home.js

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,11 +1,15 @@
-import {React, useState} from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
-import '../style/nav.css';
-import { useAuth } from "../contexts/AuthContext"
-import { useHistory } from "react-router-dom"
+
+import '../style/home.css'
+
 import Button  from 'react-bootstrap/Button';
 import logo from "../style/black logo.png"
+import img1 from '../style/backwards.png'
+import img2 from '../style/arrow.png'
+import backround from '../style/backround.jpg'
+
 
 
 
@@ -13,56 +17,27 @@ import logo from "../style/black logo.png"
 
 function Home(){
 
-    const [error, setError] = useState("")
-    const { logout } = useAuth()
-    const history = useHistory()
-  
-    async function handleLogout() {
-      setError("")
-  
-      try {
-        await logout()
-        history.push("/")
-      } catch {
-        setError("Failed to log out")
-      }
-    }
-
-
     return(
         
-        <div className="container-fluid bg-light  ">
-            <div className="text-center  ">
+        <div className="home-backround text-center  ">
             <img src={logo} alt="logo " className="logo"/>
-            <h1 className="text-center">דף הבית</h1>
+        <div className="row">
 
-        </div>
-
- 
-           
-
-        <div className="row bg-warning">
-
-            <div className="col-md-6 col-sm-12 col-xs-12 text-center ">
-                <br/>
-           <Link to="DriveReq" >
-            <Button variant="success" size="lg">
-                    בקשת נסיעה
-                </Button>{' '}
-            </Link>
-           
+            <div className="col-md-6 col-sm-6 col-xs-6 ">
+                <Link to="DriveReq" >
+                        <h5>בקשת נסיעה</h5>
+                        <br/>
+                        <img src={img1} alt="img" className="arrow" />
+                 </Link>
            </div>
 
-           <div className="col-md-6 col-sm-12 col-xs-12 text-center ">
-               <br/>
-           <Link to="Driver" >
-           <Button variant="primary" size="lg">
-                                  הצעת עזרה
-                </Button>{' '}
-            </Link>
+           <div className="col-md-6 col-sm-6 col-xs-6 ">
+                <Link to="Driver" >
+                        <h5>הצעת עזרה</h5>
+                        <br/>
+                        <img src={img2} alt="img" className="arrow"/>
+                </Link>
            </div>
-
-
         </div>
         </div>
 
