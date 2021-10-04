c4ccaef7ff0f3ae3dece7df985a7f12ee2e90c5c
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-10-04 19:21:52 +0300

    Update firebase.js

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -1,6 +1,6 @@
   
-import firebase from "firebase/app"
-import "firebase/auth"
+import firebase from "firebase/app";
+import "firebase/auth";
 import "firebase/firestore";
 
 
@@ -14,6 +14,7 @@ const app = firebase.initializeApp({
     messagingSenderId: "440499282696",
     appId: "1:440499282696:web:17d89431098132a70dc2bd"
   });
-  export const auth = app.auth()
-  export const db = firebase.firestore().collection("users")
-  export default app
+  export const auth = app.auth();
+  export const dbReq = firebase.firestore().collection("request");
+  export const db = firebase.firestore().collection("users");
+  export default app;
