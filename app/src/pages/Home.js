d672ef90f31fd34c2153f15374527f6c58177414
asih94 <asafhager94@gmail.com>
2021-10-05 12:24:19 +0300

    Update Home.js

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -4,12 +4,12 @@ import '../style/home.css'
 import logo from "../style/black logo.png"
 import { Button } from 'react-bootstrap';
 
+
 function Home(){
 
     return(
-        
-        
-        <div className="home-backround text-center">
+              
+        <div className="home-backround text-center" style = {{height:"93vh"}}>
             <img src={logo} alt="logo" className="logo"/>
 
             <div className="row justify-content-center mt-5">
@@ -17,24 +17,19 @@ function Home(){
                 <div class='is-grouped'>
 
                     <Link to="DriveReq" >
-                        <Button variant="outline-info" size="xxl">בקשת נסיעה</Button>
+                        <Button type="button" variant="outline-info mr-5" size="xxl">בקשת נסיעה</Button>
 
                     </Link>
-
 
                     <Link to="Driver">
                         <Button variant="outline-success" size="xxl">הצעת עזרה</Button>
 
                     </Link>
 
-                    </div>
-                
+
+                    </div>          
            </div>
-
-
         </div>
-
-
     )
 }
 
