56812b302674815465fe0085f8115e58849169dd
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-10-06 09:43:23 +0300

    Update Driver.js

--- app/src/pages/Driver.js
+++ app/src/pages/Driver.js
@@ -1,4 +1,4 @@
-import {createElement, React, useState} from 'react';
+import { React} from 'react';
 import Nav from "../component/Nav";
 import Table from 'react-bootstrap/Table'
 import Search from '../component/Search';
@@ -15,83 +15,106 @@ function Driver(){
 
 
     const tbody = document.getElementById('tbody1');
-   // const [user,setUser]=useState(null)
+
+
 
 
     useEffect(getdata)
     async function getdata () {
         const data = firebase.firestore().collection('request')
+        var tbody=document.getElementById("tbody1")
         
 
         await data.get().then((q) => {
             var req = [];
-            var users=[];
             q.forEach(doc=>{
                 req.push(doc.data());
 
-
-
             });
-             req.forEach(element =>{
-                const user= firebase.firestore().collection('users').doc(element.id);
-                console.log( +user.id);
-                addAllItems(req,user)
-            });
+            addAllItems(req)
             
-
-
-
-
-
           })
 
-          function additems(phone){
+          function additems(phone,dest,exit,time,date,name){
             const tr= document.createElement('tr');
             const td1= document.createElement('td');
             const td2= document.createElement('td');
+            const td3= document.createElement('td');
+            const td4= document.createElement('td');
+            const td5= document.createElement('td');
+            const td6= document.createElement('td');
+            const td7= document.createElement('td');
 
             var btn = document.createElement('input');
-            btn.type = "button";
-            btn.className = "btn btn-primary  text-center";
-            btn.value = "אשר נסיעה";
-           // btn.onclick = (function(entry) {return function() {chooseUser(entry);}})(entry);
+                btn.type = "button";
+                btn.className = "btn btn-primary  text-center";
+                btn.value = "אשר נסיעה";
+               // btn.onclick = RideApproval(phone,dest,exit,time,date,name);
             td1.appendChild(btn);
             td2.innerHTML=phone;
+            td3.innerHTML=dest;
+            td4.innerHTML=exit;
+            td5.innerHTML=time;
+            td6.innerHTML=date;
+            td7.innerHTML=name;
+            tr.appendChild(td1);
             tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tr.appendChild(td6);
+            tr.appendChild(td7);
+
+            
               
-              tr.appendChild(td1);
+              
               tbody.appendChild(tr)
           }
-          function addAllItems(reqList,user){
-              tbody.innerHTML="עע";
-              user.forEach(ditels=>{
-                additems(ditels.phone_number)
-              })
-            //   reqList.forEach(element =>{
-            //       additems(element.exit)
-            //   })
-          }
+            function addAllItems(reqData){
+            tbody.innerHTML="";
+                var info='';
+            reqData.forEach(element =>{
 
+                 firebase.firestore().collection('users').doc( element.id)
+                .get()
+                .then((value)=> {
+                    info=value.data()
+                    console.log(info)
+                    getuser(info,element)
+                } )
+                 console.log("var"+info)
 
-
- 
-          
-
-        //   for(const row of data) {
-        //       const tr = document.createElement("tr");
-
-        //       for(const cell of row){
-        //           const td = document.createElement("td");
-        //           td.textContent = cell;
-        //           tr.appendChild(td);
-        //       };
               
-        //       tblBody.appendChild(tr);
-        //   };
-          
+          })
+ 
+        }
+        function getuser(user,req){
+            let phone =user.phone_number
+            let dest = req.destination
+            let exit = req.exit
+            let time = req.time
+            let date =req.date
+            let name = user.first_name +" "+user.last_name
+            additems(phone,dest,exit,time,date,name)
+        }
 
+        function RideApproval(phone,dest,exit,time,date,name){
+             const Confirmation = firebase.firestore().collection("myDrive");
+             Confirmation.doc().set({
+                name:name,
+                phone:phone,
+                dest:dest,
+                ext:exit,
+                time:time,
+                data:data
+              })
 
         }
+
+    }
+
+
+
           
 
 
