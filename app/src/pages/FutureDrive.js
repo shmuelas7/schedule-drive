934b49279d725ac3205c2cb127e5c0188d24b35c
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-10-06 13:04:41 +0300

    Update FutureDrive.js

--- app/src/pages/FutureDrive.js
+++ app/src/pages/FutureDrive.js
@@ -2,10 +2,42 @@ import React from 'react';
 import Nav from "../component/Nav";
 import Table from 'react-bootstrap/Table'
 import Search from '../component/Search';
+import { useEffect } from 'react';
+import firebase from 'firebase';
 
   
 
 function FutureDrive(){
+
+    var tbody = document.getElementById('tbody1');
+
+    useEffect(getdata)
+    async function getdata () {
+        const data = firebase.firestore().collection('myDrive')
+        
+        
+
+        await data.get().then((q) => {
+            var drive = [];
+            q.forEach(doc=>{
+                drive.push(doc.data());
+
+            });
+            addToTable(drive)
+            
+          })
+
+  
+
+        }
+        function addToTable(drive){
+            //tbody.innerHTML="";
+            var info='';
+        drive.forEach(element =>{
+            })
+        }
+              
+
     return(
         <div className="container-fluid " >
             
