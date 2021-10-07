ef4fe89bca0061b69cb38e80b7956a4be44d9a70
asih94 <asafhager94@gmail.com>
2021-10-07 11:11:01 +0300

    Update CardProfile.js

--- app/src/pages/CardProfile.js
+++ app/src/pages/CardProfile.js
@@ -13,22 +13,38 @@ function CardProfile(){
         <div className="bg-warning">
             <h1 className="text-center">דף משתמש</h1>
 
-            <div class="container d-flex justify-content-center mt-5">
+            <div class="container mt-3">
                 <div class="card">
                     <div class="top-container"> 
-                        <img src="https://i.imgur.com/G1pXs7D.jpg" class="img-fluid profile-image" width="90">
+                        <img src="https://i.imgur.com/G1pXs7D.jpg" class="img-fluid profile-image" width="100">
                         </img>
                         <div class="ml-3">
                             <h2 class="name">יוסי כהן</h2>
-                            <p class="mail">yossi@gmail.com</p>
+                            <p class="mail">25</p>
+                            <p class="mail">פתח תקווה</p>
                             <p class="mail">05236416497</p>
+                            <p class="mail">yossi@gmail.com</p>
                         </div>
                     </div>
                     
                     <h3 class="mt-4">ביקורות</h3>
+                    <div class="w3-container">
+                        <ul>
+                            <li><b>איציק :</b> הגיע בזמן ומאוד עזר לי</li>
+                            <li><b>לאה :</b> אחלה נהג</li>
+                            <li><b>דני :</b> ממליץ מאוד לנסוע איתו</li>
+                            </ul>
+                            </div>  
+                            <div className="text-right">                          
+                        <textarea className="form-control" rows="1"></textarea>
+                        </div>
+                        <button>הוסף ביקורת</button>
                 </div>
             </div>
-            
+
+            <br></br>
+            <br></br>
+
         </div>
     </div>
     )
