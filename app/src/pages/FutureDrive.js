8ba7b69e0ba980ea47a060f47cc408bb3305c99b
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-10-10 23:54:24 +0300

    Update FutureDrive.js

--- app/src/pages/FutureDrive.js
+++ app/src/pages/FutureDrive.js
@@ -4,38 +4,108 @@ import Table from 'react-bootstrap/Table'
 import Search from '../component/Search';
 import { useEffect } from 'react';
 import firebase from 'firebase';
+import { useAuth } from "../contexts/AuthContext"
 
   
 
 function FutureDrive(){
 
-    var tbody = document.getElementById('tbody1');
+    const { currentUser } = useAuth();
+    var ask="";
+    var driver="";
 
     useEffect(getdata)
-    async function getdata () {
-        const data = firebase.firestore().collection('myDrive')
-        
-        
 
-        await data.get().then((q) => {
+    async function getdata () {
+        var tbody = document.getElementById('tbody1');
+        const data =   firebase.firestore().collection('request')
+        data.where('id_driver','!=', null )
+        .where('id_driver', '==', currentUser.uid )
+        //.where('date','>',Date.now())
+        .get().then((q) => {
             var drive = [];
             q.forEach(doc=>{
                 drive.push(doc.data());
+                console.log(doc.data())
 
             });
-            addToTable(drive)
+           setTimeout(addToTable(drive),500000)
+        })
+    }
+
+        async function addToTable(drive){
             
-          })
-
-  
-
-        }
-        function addToTable(drive){
-            //tbody.innerHTML="";
-            var info='';
-        drive.forEach(element =>{
+        var tbody = document.getElementById('tbody1');
+            tbody.innerHTML="";
+        
+        await drive.forEach(element =>{
+            var x="";
+            var y=";"
+            console.log(element.id_ask)
+           firebase.firestore().collection('users').doc(element.id_ask)
+            .get().then((as)=>{
+                    x =as.data()
+                    getuser1(x)
+        })
+        console.log(element.id_driver)
+             firebase.firestore().collection('users').doc(element.id_driver)
+            .get().then((driv)=>{
+                y = driv.data();
+                getuser2(y)
+            })
+            add(element)
             })
         }
+        function add(data){
+            console.log("check "+ask.age)
+            var tbody = document.getElementById('tbody1');
+            const tr= document.createElement('tr');
+            const td1= document.createElement('td');
+            const td2= document.createElement('td');
+            const td3= document.createElement('td');
+            const td4= document.createElement('td');
+            const td5= document.createElement('td');
+            const td6= document.createElement('td');
+            const td7= document.createElement('td');
+
+            const btn = document.createElement('input');
+                
+                btn.type = "button";
+                btn.className = "btn btn-primary  text-center";
+                btn.value = "בטל נסיעה";
+                //btn.onclick = RideApproval;
+                
+            td2.innerHTML=driver.first_name;
+            td3.innerHTML=data.dest;
+            td4.innerHTML=data.exit;
+            td5.innerHTML=data.time;
+            td6.innerHTML=data.date;
+           // td7.innerHTML=name;
+            
+            td1.appendChild(btn);
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tr.appendChild(td6);
+            tr.appendChild(td7);
+
+            
+              
+              
+            tbody.appendChild(tr)
+        }
+        function getuser1(x){
+            ask=x;
+            console.log(ask.first_name)
+        }
+
+        function getuser2(x){
+            driver=x;
+            console.log(driver.first_name)
+        }
+        
               
 
     return(
@@ -46,7 +116,7 @@ function FutureDrive(){
             <div className="bg-warning">
                 <h1 className="text-center">הנסיעות שלי</h1>
                 <Search/>
-                <Table striped bordered hover variant="dark">
+                <Table striped bordered hover variant="dark" responsive>
                 <thead className="text-right">
                     <tr>
                         <th>עריכת נסיעה</th>
@@ -60,7 +130,7 @@ function FutureDrive(){
                         <th>שם המבקש</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody id="tbody1">
                 </tbody>
             </Table>
             </div>
