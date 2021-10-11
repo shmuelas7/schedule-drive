3bb3791ef6244060e08515e748eb8b1d863a25f2
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-10-11 22:03:40 +0300

    Update PreviousDrive.js

--- app/src/pages/PreviousDrive.js
+++ app/src/pages/PreviousDrive.js
@@ -2,9 +2,132 @@ import React from 'react';
 import Nav from "../component/Nav";
 import Table from 'react-bootstrap/Table'
 import Search from '../component/Search';
+import { useEffect } from 'react';
+import firebase from 'firebase';
+import { useAuth } from "../contexts/AuthContext"
   
 
 function PreviousDrive(){
+    const { currentUser } = useAuth();
+    var ask="";
+    var driver="";
+
+    useEffect(getdata)
+
+    async function getdata () {
+        const data =   firebase.firestore().collection('request')
+        data.where('id_driver','!=', null )
+        .where('id_driver', '==', currentUser.uid )
+        .get().then((q) => {
+            var drive = [];
+            q.forEach(doc=>{
+                let x= doc.data()
+                var today = new Date();
+                var dd = String(today.getDate()).padStart(2, '0');
+                var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+                var yyyy = today.getFullYear();
+                today = mm + '/' + dd + '/' + yyyy;
+                if(x.date < today)
+                {
+                    console.log(today)
+                    drive.push(doc.data());
+                }
+                console.log(doc.data())
+
+            });
+            
+                addToTable(drive)
+            })
+        
+    }
+
+        async function addToTable(drive){
+            
+        var tbody = document.getElementById('tbody1');
+            tbody.innerHTML="";
+        
+        await drive.forEach(element =>{
+            var x="";
+            var y=";"
+            console.log(element.id_ask)
+           firebase.firestore().collection('users').doc(element.id_ask)
+            .get().then((as)=>{
+                    x =as.data()
+                    getuser1(x)
+        })
+        console.log(element.id_driver)
+             firebase.firestore().collection('users').doc(element.id_driver)
+            .get().then((driv)=>{
+                y = driv.data();
+                getuser2(y)
+            })
+            setTimeout(() => {
+                add(element)}, 2000)
+            })
+        }
+        function add(data){
+            console.log("check "+ask.age)
+            var tbody = document.getElementById('tbody1');
+            const tr= document.createElement('tr');
+            const td1= document.createElement('td');
+            const td2= document.createElement('td');
+            const td3= document.createElement('td');
+            const td4= document.createElement('td');
+            const td5= document.createElement('td');
+            const td6= document.createElement('td');
+            const td7= document.createElement('td');
+            const btn = document.createElement('input');
+                
+                btn.type = "button";
+                btn.className = "btn btn-primary  text-center";
+                btn.value = "דרג";
+                //btn.onclick = RideApproval;
+                
+
+
+
+            td2.innerHTML=data.destination;
+            td2.className="text-right"
+
+            td3.innerHTML=data.exit;
+            td3.className="text-right"
+
+            td4.innerHTML=data.time;
+            td4.className="text-right"
+
+            td5.innerHTML=data.date;
+            td5.className="text-right"
+
+            td6.innerHTML=ask.first_name;
+            td6.className="text-right"
+
+            td7.innerHTML=driver.first_name;
+            td7.className="text-right"
+            
+            td1.appendChild(btn);
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tr.appendChild(td6);
+            tr.appendChild(td7);
+
+
+            
+              
+              
+            tbody.appendChild(tr)
+        }
+        function getuser1(x){
+            ask=x;
+            console.log(ask.first_name)
+        }
+
+        function getuser2(x){
+            driver=x;
+            console.log(driver.first_name)
+        }
     return(
         <div className="container-fluid " >
             <div className=" bg-warning">
@@ -15,15 +138,15 @@ function PreviousDrive(){
                 <thead className="text-right">
                     <tr>
                     <th>דירוג נהג / נוסע</th>
-                    <th>מספר טלפון</th>
                     <th>יעד</th>
                     <th>מוצא</th>
                     <th>שעה</th>
                     <th>תאריך</th>
-                    <th>שם המבקש / נהג</th>
+                    <th>שם המבקש</th>
+                    <th>שם הנהג</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody id="tbody1">
                 </tbody>
             </Table>
             </div>
