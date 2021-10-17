2278c61656e16718069669ef61aeba0c8bc389f5
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-10-17 22:16:11 +0300

    Update Home.js

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -2,31 +2,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import '../style/home.css'
 import logo from "../style/black logo.png"
+import Button  from 'react-bootstrap/Button';
+
 
 function Home(){
 
     return(
-
-        <div className="home-backround text-center" style = {{height:"93.0vh"}}>
-            <img src={logo} alt="logo" className="logo"/>
+    <div className="p-3  bg-primary">
+        <div className=" text-center" style = {{height:"90.0vh"}}>
+            <img src={logo} alt="logo" className="logo mb-5"/>
 
             <div className="row justify-content-center mt-5">
 
-                <div className='is-grouped' style = {{margin:"75px"}} >
-
+                <div className='is-grouped col-lg-6 col-md-6 col-sm-12 '>
                     <Link to="DriveReq" >
-
-                        <button class="btn1 mr-5">בקשת נסיעה</button>
-
+                        <Button className=" btn-xxl mb-4 btn btn-light" block>בקשת נסיעה</Button>
                     </Link>
-
+                </div>
+                <div className='is-grouped col-lg-6 col-md-6 col-sm-12 '>
                     <Link to="Driver">
-                        <button class="btn2">הצעת עזרה</button>
+                        <Button className="  btn-xxl btn btn-light " block>הצעת עזרה</Button>
                     </Link>
-
-                    </div>          
-           </div>
+                </div>
+                     
         </div>
+    </div>
+    </div>
 
     )
 }
