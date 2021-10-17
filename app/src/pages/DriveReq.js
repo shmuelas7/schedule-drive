246624b4b5e6aa9979bf8794301f6469b53e2a48
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-10-17 22:16:04 +0300

    Update DriveReq.js

--- app/src/pages/DriveReq.js
+++ app/src/pages/DriveReq.js
@@ -13,8 +13,33 @@ const [date , setdate]= useState(Date);
 const [exit , setexit]= useState("");
 const [destination , setdestination]= useState("");
 const [time,settime]= useState("");
+const [comment,setComment]=useState("")
+
+var today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+const yyyy = today.getFullYear();
+ today = mm + '/' + dd + '/' + yyyy;
+
+ function validation(){
+     if(date> today)
+     return true
+        else
+        {
+            swal.fire({
+                icon: 'error',
+                title: 'שגיאה',
+                text: 'תאריך לא תקין ',
+                confirmButtonText: 'בסדר',
+              })
+            return  false
+        } 
+ }
+
 
 function handleSubmit(e) {
+
+    if(validation)
     e.preventDefault();
     console.log(currentUser.uid);
     dbReq.doc(currentUser.uid).set({
@@ -22,7 +47,9 @@ function handleSubmit(e) {
         exit:exit,
         destination:destination,
         time:time,
-        id_ask:currentUser.uid
+        id_ask:currentUser.uid,
+        id_driver:null,
+        comment:comment
      })
      .then(() => {
          console.log("Document successfully written!");
@@ -42,7 +69,7 @@ function handleSubmit(e) {
                     <form className=" border border-dark rounded mt-2 mb-2 bg-light"  onSubmit={handleSubmit} >
                         <h1 className="text-center">בקשת נסיעה</h1>
                         <h6 className="text-right">הכנס תאריך</h6>
-                        <input type="date" placeholder="תאריך יציאה" className="form-control input-d" data-date-format="mm/dd/yyyy" onChange={(e)=>setdate(e.target.value)} required></input>
+                        <input type="date" placeholder="תאריך יציאה" className="form-control input-d" data-date-format="yyy/dd/mm" onChange={(e)=>setdate(e.target.value)} required></input>
                         <h6 className="text-right">מוצא</h6>
                         <input placeholder="מוצא" className="form-control text-right" onChange={(e)=>setexit(e.target.value)} required ></input>
                         <h6 className="text-right">יעד</h6>
@@ -51,7 +78,7 @@ function handleSubmit(e) {
                         <input  type="time" placeholder="שעה" className="form-control" onChange={(e)=>settime(e.target.value)} required ></input>
                         <div className="text-right">
                             <h6>הערות</h6>
-                            <textarea className="form-control" rows="2"></textarea>
+                            <textarea className="form-control" rows="2" onChange={(e)=>setComment(e.target.value)}></textarea>
                         </div>
                         
                         <div className="text-right">
