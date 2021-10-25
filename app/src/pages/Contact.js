61a6026b21ea7b4b4f08b48ccbc5c78e36d613e2
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-10-25 23:30:48 +0300

    Update Contact.js

--- app/src/pages/Contact.js
+++ app/src/pages/Contact.js
@@ -1,11 +1,46 @@
-import {React} from 'react';
+import {React, useState} from 'react';
 import '../style/Contact.css';
 import logo from '../style/black logo.png'
+import Button from 'react-bootstrap/Button';
+import firebase from 'firebase';
+import swal from 'sweetalert2'
 
 function Contact(){
 
-
+	const [name,setName]=useState("")
+	const [mail,setMail]=useState("")
+	const [subject,setSubject]=useState("")
+	const [messege,setMessege]=useState("")
 	
+	const data =   firebase.firestore().collection('contact')
+
+
+	async function sentData(){
+        await data.set({
+        name:name,
+        mail:mail,
+        subject:subject,
+        messege:messege,
+      })
+      .then(() => {
+          console.log("Document successfully written!");
+          swal.fire("הבקשה נקלטה במערכת ", "success")
+          
+      })
+      .catch((error) => {
+          console.error("Error writing document: ", error);
+      });
+
+	}
+
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		sentData()
+	}
+
+
+
     return(
      <div class="contact1 ">
 		<div class="container-contact1">
@@ -13,35 +48,33 @@ function Contact(){
 				<img src={logo} alt="IMG"/>
 			</div>
 
-			<form class="contact1-form validate-form">
+			<form class="contact1-form validate-form" onSubmit={handleSubmit}>
 				<span class="contact1-form-title">
 					יצירת קשר
 				</span>
 
 				<div class="wrap-input1 validate-input" data-validate = "Name is required">
-					<input class="input1 text-right" type="text" name="name" placeholder="שם" />
+					<input class="input1 text-right" type="text" name="name" placeholder="שם" onChange={(e)=>setName(e.target.value)}/>
 					<span class="shadow-input1"></span>
 				</div>
 
 				<div class="wrap-input1 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-					<input class="input1 text-right" type="text" name="email" placeholder="מייל"/>
+					<input class="input1 text-right" type="text" name="email" placeholder="מייל" onChange={(e)=>setMail(e.target.value)}/>
 					<span class="shadow-input1"></span>
 				</div>
 
 				<div class="wrap-input1 validate-input" data-validate = "Subject is required">
-					<input class="input1 text-right" type="text" name="subject" placeholder="נושא"/>
+					<input class="input1 text-right" type="text" name="subject" placeholder="נושא"onChange={(e)=>setSubject(e.target.value)}/>
 					<span class="shadow-input1"></span>
 				</div>
 
 				<div class="wrap-input1 validate-input" data-validate = "Message is required">
-					<textarea class="input1 text-right" name="message" placeholder="הודעה"></textarea>
+					<textarea class="input1 text-right" name="message" placeholder="הודעה" onChange={(e)=>setMessege(e.target.value)}></textarea>
 					<span class="shadow-input1"></span>
 				</div>
 
 				<div class="container-contact1-form-btn">
-					<button class="contact1-form-btn">
-							שלח
-					</button>
+					<Button type="submit" class="contact1-form-btn">שלח</Button>
 				</div>
 			</form>
 		</div>
