7340b235458c65c7c92a5764c766722fa372ab92
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-11-13 22:46:04 +0200

    Update Taxi.js

--- app/src/pages/Taxi.js
+++ app/src/pages/Taxi.js
@@ -30,7 +30,6 @@ function Taxi(){
             <Nav/>
             <div className="row  bg-warning">
             <div className="col-md-2 col-sm-12 col-xs-12 "></div>
-            <div className="col-md-8 col-sm-12 col-xs-12">
 
                     <h1 className="text-center"> הזמנת מוניות </h1>
 
@@ -51,10 +50,10 @@ function Taxi(){
                                         show1?<h4>*3242</h4>:null
                                     }
 
-                                </div>
+                            </div>
 
 
-                                <div className="col-md-2 col-sm-12 col-xs-12 text-center">
+                            <div className="col-md-2 col-sm-12 col-xs-12 text-center">
                                 <a href="https://www.taxitogo.co.il/%D7%9E%D7%95%D7%A0%D7%99%D7%95%D7%AA-%D7%92%D7%91%D7%A2%D7%AA%D7%99%D7%99%D7%9D-%D7%98%D7%A7%D7%A1%D7%99-%D7%98%D7%95-%D7%92%D7%95-taxitogo/"> 
                                     <img src={giva} alt="img" className="icon1"/>
                                 </a>
@@ -65,11 +64,11 @@ function Taxi(){
                                     {
                                         show2?<h4>058-5007008</h4>:null
                                     }
-                                </div>
+                            </div>
 
 
 
-                                <div className="col-md-2 col-sm-12 col-xs-12 text-center">
+                            <div className="col-md-2 col-sm-12 col-xs-12 text-center">
                                 <a href="https://taxi-shoham.co.il/%D7%AA%D7%97%D7%A0%D7%AA-%D7%9E%D7%95%D7%A0%D7%99%D7%95%D7%AA-%D7%91%D7%A9%D7%95%D7%94%D7%9D/"> 
                                     <img src={shoham} alt="img" className="icon1"/>
                                 </a>
@@ -81,10 +80,10 @@ function Taxi(){
                                         show3?<h4>03-3818000</h4>:null
                                     }
 
-                                </div>
+                            </div>
 
 
-                                <div className="col-md-2 col-sm-12 col-xs-12 text-center">
+                            <div className="col-md-2 col-sm-12 col-xs-12 text-center">
                                 <a href="https://stars-taxi.co.il/"> 
                                     <img src={stars} alt="img" className="icon1"/>
                                 </a>
@@ -96,10 +95,10 @@ function Taxi(){
                                     {
                                         show4?<h4>08-9106106</h4>:null
                                     }
-                                </div>
+                            </div>
 
 
-                                <div className="col-md-2 col-sm-12 col-xs-12 text-center">
+                            <div className="col-md-2 col-sm-12 col-xs-12 text-center">
                                 <a href="http://www.taxik8.com/%D7%90%D7%99%D7%A0%D7%93%D7%A7%D7%A1-%D7%A9%D7%9C-%D7%9E%D7%95%D7%A0%D7%99%D7%95%D7%AA-%D7%A7%D7%A8%D7%99%D7%AA-%D7%A9%D7%9E%D7%95%D7%A0%D7%94/"> 
                                     <img src={k8} alt="img" className="icon1"/>
                                 </a>
@@ -112,10 +111,10 @@ function Taxi(){
                                         show5?<h4>052-9121933</h4>:null
                                     }
 
-                                </div>
+                            </div>
 
 
-                                <div className="col-md-2 col-sm-12 col-xs-12 text-center">
+                            <div className="col-md-2 col-sm-12 col-xs-12 text-center">
                                 <a href="https://www.taxys.info/"> 
                                     <img src={itz} alt="img" className="icon1"/>
                                 </a>
@@ -128,14 +127,11 @@ function Taxi(){
                                         show6?<h4>054-4641726</h4>:null
                                     }
 
-                                </div>
-
-
                             </div>
+                        </div>
 
 
                  </div>  
-            </div>
             </div>
             
         </div>
