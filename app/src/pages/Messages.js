17c8abd565f7f7a08a38d9deb9604ff1eaf25111
asih94 <asafhager94@gmail.com>
2021-11-13 22:44:47 +0200

    Create Messages.js

---
+++ app/src/pages/Messages.js
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+import '../style/manager.css'
+import logo from "../style/black logo.png"
+import mes from "../icon/mes.png";
+import list from "../icon/list.png";
+
+
+function Manager(){
+
+    return(
+    <div className="p-3  bg-info">
+        <div className=" text-center" style = {{height:"90.0vh"}}>
+            <img src={logo} alt="logo" className="logo mb-5"/>
+
+            <h1>הודעות נכנסות </h1>
+
+    </div>
+    </div>
+
+    )
+}
+
+export default Manager;
