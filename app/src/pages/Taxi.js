95cb8730e19b78a205e18169a149810419b4fe42
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-11-14 20:57:08 +0200

    Update Taxi.js

--- app/src/pages/Taxi.js
+++ app/src/pages/Taxi.js
@@ -29,112 +29,84 @@ function Taxi(){
         <div className="container-fluid">
             <Nav/>
             <div className="row  bg-warning">
-            <div className="col-md-2 col-sm-12 col-xs-12 "></div>
-
+                <div className="col-md-12 col-sm-12 col-xs-12 text-center">
                     <h1 className="text-center"> הזמנת מוניות </h1>
+                </div>
 
-                    <div class="center1">
-                        <h5>בהזמנת מונית מאחת התחנות הללו דרך האתר ישנה הנחה של 10%</h5>
-
-                        <div className="row">
-
-
-                            <div className="col-md-2 col-sm-12 col-xs-12 text-center">
-                                <a href="https://www.340.co.il/Pages/KastelTaxi.php?sID=152&dID=8&kID=1066"> 
-                                    <img src={castel} alt="img" className="icon1"/>
-                                </a>
-                                <br/>
-                                   
-                                    <button onClick={()=>setShow1(!show1)}>הצגת מספר</button>
-                                    {
-                                        show1?<h4>*3242</h4>:null
-                                    }
-
-                            </div>
-
-
-                            <div className="col-md-2 col-sm-12 col-xs-12 text-center">
-                                <a href="https://www.taxitogo.co.il/%D7%9E%D7%95%D7%A0%D7%99%D7%95%D7%AA-%D7%92%D7%91%D7%A2%D7%AA%D7%99%D7%99%D7%9D-%D7%98%D7%A7%D7%A1%D7%99-%D7%98%D7%95-%D7%92%D7%95-taxitogo/"> 
-                                    <img src={giva} alt="img" className="icon1"/>
-                                </a>
-                                <br/>
-
-                                    <button onClick={()=>setShow2(!show2)}>הצגת מספר</button>
-
-                                    {
-                                        show2?<h4>058-5007008</h4>:null
-                                    }
-                            </div>
-
-
-
-                            <div className="col-md-2 col-sm-12 col-xs-12 text-center">
-                                <a href="https://taxi-shoham.co.il/%D7%AA%D7%97%D7%A0%D7%AA-%D7%9E%D7%95%D7%A0%D7%99%D7%95%D7%AA-%D7%91%D7%A9%D7%95%D7%94%D7%9D/"> 
-                                    <img src={shoham} alt="img" className="icon1"/>
-                                </a>
-                                <br/>
-                                   
-                                    <button onClick={()=>setShow3(!show3)}>הצגת מספר</button>
-
-                                    {
-                                        show3?<h4>03-3818000</h4>:null
-                                    }
-
-                            </div>
-
-
-                            <div className="col-md-2 col-sm-12 col-xs-12 text-center">
-                                <a href="https://stars-taxi.co.il/"> 
-                                    <img src={stars} alt="img" className="icon1"/>
-                                </a>
-
-                                    <br/>
-
-                                    <button onClick={()=>setShow4(!show4)}>הצגת מספר</button>
-
-                                    {
-                                        show4?<h4>08-9106106</h4>:null
-                                    }
-                            </div>
-
-
-                            <div className="col-md-2 col-sm-12 col-xs-12 text-center">
-                                <a href="http://www.taxik8.com/%D7%90%D7%99%D7%A0%D7%93%D7%A7%D7%A1-%D7%A9%D7%9C-%D7%9E%D7%95%D7%A0%D7%99%D7%95%D7%AA-%D7%A7%D7%A8%D7%99%D7%AA-%D7%A9%D7%9E%D7%95%D7%A0%D7%94/"> 
-                                    <img src={k8} alt="img" className="icon1"/>
-                                </a>
-
-                                    <br/>
-                                  
-                                    <button onClick={()=>setShow5(!show5)}>הצגת מספר</button>
-
-                                    {
-                                        show5?<h4>052-9121933</h4>:null
-                                    }
-
-                            </div>
-
-
-                            <div className="col-md-2 col-sm-12 col-xs-12 text-center">
-                                <a href="https://www.taxys.info/"> 
-                                    <img src={itz} alt="img" className="icon1"/>
-                                </a>
-
-                                    <br/>
-                                  
-                                    <button onClick={()=>setShow6(!show6)}>הצגת מספר</button>
-
-                                    {
-                                        show6?<h4>054-4641726</h4>:null
-                                    }
-
-                            </div>
-                        </div>
-
-
-                 </div>  
+                <div className="col-md-12 col-sm-12 col-xs-12 text-center">
+                    <h5>בהזמנת מונית מאחת התחנות הללו דרך האתר ישנה הנחה של 10%</h5>
+                </div>
             </div>
-            
-        </div>
+            <div className="row bg-warning">
+
+                <div className="col-md-2 col-sm-12 col-xs-12 text-center">
+                    <a href="https://www.340.co.il/Pages/KastelTaxi.php?sID=152&dID=8&kID=1066"> 
+                        <img src={castel} alt="img" className="icon1"/>
+                    </a>
+                    <br/>
+                    <button onClick={()=>setShow1(!show1)}>הצגת מספר</button>
+                        {
+                            show1?<h4>*3242</h4>:null
+                        }
+                </div>
+
+                <div className="col-md-2 col-sm-12 col-xs-12 text-center">
+                    <a href="https://www.taxitogo.co.il/%D7%9E%D7%95%D7%A0%D7%99%D7%95%D7%AA-%D7%92%D7%91%D7%A2%D7%AA%D7%99%D7%99%D7%9D-%D7%98%D7%A7%D7%A1%D7%99-%D7%98%D7%95-%D7%92%D7%95-taxitogo/"> 
+                        <img src={giva} alt="img" className="icon1"/>
+                    </a>
+                    <br/>
+                    <button onClick={()=>setShow2(!show2)}>הצגת מספר</button>
+                        {
+                            show2?<h4>058-5007008</h4>:null
+                        }
+                </div>
+
+                <div className="col-md-2 col-sm-12 col-xs-12 text-center">
+                    <a href="https://taxi-shoham.co.il/%D7%AA%D7%97%D7%A0%D7%AA-%D7%9E%D7%95%D7%A0%D7%99%D7%95%D7%AA-%D7%91%D7%A9%D7%95%D7%94%D7%9D/"> 
+                        <img src={shoham} alt="img" className="icon1"/>
+                    </a>
+                    <br/>
+                    <button onClick={()=>setShow3(!show3)}>הצגת מספר</button>
+                        {
+                            show3?<h4>03-3818000</h4>:null
+                        }
+                </div>
+
+                <div className="col-md-2 col-sm-12 col-xs-12 text-center">
+                    <a href="https://stars-taxi.co.il/"> 
+                        <img src={stars} alt="img" className="icon1"/>
+                    </a>
+                    <br/>
+                    <button onClick={()=>setShow4(!show4)}>הצגת מספר</button>
+                        {
+                            show4?<h4>08-9106106</h4>:null
+                        }
+                </div>
+                
+                <div className="col-md-2 col-sm-12 col-xs-12 text-center">
+                    <a href="http://www.taxik8.com/%D7%90%D7%99%D7%A0%D7%93%D7%A7%D7%A1-%D7%A9%D7%9C-%D7%9E%D7%95%D7%A0%D7%99%D7%95%D7%AA-%D7%A7%D7%A8%D7%99%D7%AA-%D7%A9%D7%9E%D7%95%D7%A0%D7%94/"> 
+                        <img src={k8} alt="img" className="icon1"/>
+                    </a>
+                    <br/>
+                    <button onClick={()=>setShow5(!show5)}>הצגת מספר</button>
+                        {
+                            show5?<h4>052-9121933</h4>:null
+                        }
+                </div>
+
+                <div className="col-md-2 col-sm-12 col-xs-12 text-center">
+                    <a href="https://www.taxys.info/"> 
+                        <img src={itz} alt="img" className="icon1"/>
+                    </a>
+                    <br/>
+                    <button onClick={()=>setShow6(!show6)}>הצגת מספר</button>
+                        {
+                        show6?<h4>054-4641726</h4>:null
+                        }
+                </div>
+            </div>
+        
+    </div>
     );
 }
 export default Taxi;
