d8c9d30ba0dc059908c1369d2e6bbe68a6375244
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-11-14 20:56:40 +0200

    Update Associations.js

--- app/src/pages/Associations.js
+++ app/src/pages/Associations.js
@@ -17,41 +17,42 @@ function Associations(){
        
         <div className="container-fluid">
             <Nav/>
-            <div className="row  bg-warning">
-            <div className="col-md-2 col-sm-12 col-xs-12 "></div>
-            <div className="col-md-8 col-sm-12 col-xs-12">
 
+            <div className="row bg-warning" >
+                <div className="col-md-12 col-sm-12 col-xs-12">
                     <h1 className="text-center"> ארגונים רלוונטיים</h1>
-
-                    <div class="center">
-                        <h5>בלחיצה על האייקון תועבר לדף העמותה</h5>
-
-                            <a href="https://www.hayim.org.il/"> 
-                                <img src={haim} alt="img" className="icon1"/>
-                            </a>
-
-                            <a href="https://rachasheilev.org/"> 
-                                <img src={lev} alt="img" className="icon1"/>
-                            </a>
-
-                            <a href="https://donations.cancer.org.il/"> 
-                                <img src={war} alt="img" className="icon1"/>
-                            </a>
-
-
-                            <a href="http://darcheimiriam.org.il/"> 
-                                <img src={miryam} alt="img" className="icon1"/>
-                            </a>
-
-                            <a href="https://gdolim.org.il/"> 
-                                <img src={gdolim} alt="img" className="icon1"/>
-                            </a>
-
-
-                 </div>  
+                </div>
+                <div className="col-md-12 col-sm-12 col-xs-12">
+                    <h6 className="text-right">בלחיצה על האייקון תועבר לדף העמותה</h6>
+                 </div>
             </div>
-            </div>
-            <div className="col-md-2 col-sm-12 col-xs-12 "></div>
+            <div className="row  bg-warning text-center">
+
+
+                <div className="col-md-3 col-sm-12 col-xs-12">
+                    <a href="https://www.hayim.org.il/"> 
+                        <img src={haim} alt="img" className="icon1"/>
+                    </a>
+                </div>
+
+                <div className=" col-md-3 col-sm-12 col-xs-12">
+                    <a href="https://rachasheilev.org/"> 
+                        <img src={lev} alt="img" className="icon1"/>
+                    </a>
+                </div>  
+
+                <div className=" col-md-3 col-sm-12 col-xs-12">
+                     <a href="https://donations.cancer.org.il/"> 
+                        <img src={war} alt="img" className="icon1"/>
+                    </a>
+                </div>
+
+                <div className=" col-md-3 col-sm-12 col-xs-12">    
+                    <a href="http://darcheimiriam.org.il/"> 
+                        <img src={miryam} alt="img" className="icon1"/>
+                    </a>
+                </div>  
+            </div>  
         </div>
     );
 }
