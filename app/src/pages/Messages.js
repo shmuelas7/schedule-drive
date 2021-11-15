9fc1e76aed8b949e626d660981ea8528a1a751a2
shmuelas7 <61385837+shmuelas7@users.noreply.github.com>
2021-11-15 19:03:58 +0200

    Update Messages.js

--- app/src/pages/Messages.js
+++ app/src/pages/Messages.js
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 import {dbContact} from '../firebase'
 import Button  from 'react-bootstrap/Button';
 import '../style/Messages.css';
+import swal from 'sweetalert2'
 
 
 function Manager(){
@@ -26,25 +27,81 @@ function Manager(){
         console.log("ok")
         createCard(messege)
     }
+   
     function createCard(mess){
         
-    var card = document.querySelector(".container-cardd")
+        var card = document.querySelector(".container-cardd")
         mess.forEach(elment=>{
-            let code=`      
-            <div class="card">
-            <div class="card-block">
-                <h4 class="card-title">${elment.subject}</h4>
-                    <p class="card-text">${elment.messege}</p>
-                    <p class "card-text text-left>${'שם השולח:'+elment.name}</p>
-                    <Button  class="w-100 btn btn-primary" type="submit">טופל</Button>
-            </div>
-        </div>`;
-        card.innerHTML += code
+
+            var div=document.createElement('div');
+            div.className='card mb-3';
+
+            var div2=document.createElement('div')
+            div2.className="card-block"
+
+            var h4  =document.createElement('h4')
+            h4.className="card-title"
+            h4.innerHTML=elment.subject
+
+            var p1  =document.createElement('p')
+            p1.className="card-text"
+            p1.innerHTML=elment.messege
+
+            var p2  =document.createElement('p')
+            p2.className="card-text text-right"
+            p2.innerHTML='שם השולח:'+elment.name
+
+            var btn1= document.createElement('input')
+            btn1.type="button"
+            btn1.className="w-35 btn btn-danger"
+            btn1.value="טופל"
+            btn1.onclick = (e) => {
+                finish(elment.id)
+              };
+              var btn2= document.createElement('input')
+              btn2.type="button"
+              btn2.className="w-35 btn btn-info ml-5"
+              btn2.value="תגובה"
+              btn2.onclick = (e) => {
+                  hendel(elment.mail)
+                };
+            
+            div2.appendChild(h4)
+            div2.appendChild(p1)
+            div2.appendChild(p2)
+            div2.appendChild(btn1)
+            div2.appendChild(btn2)
+            div.appendChild(div2)
+            card.appendChild(div)
+
+
 
         })
+        function finish(id){
+            console.log("finis")
+            swal.fire({
+                title:' מחיקת אירוע',
+                text:   '  אם סיימתה לטפל ? לחץ על מחק אחרת לחץ על ביטול',
+                icon:'success',
+                confirmButtonText: 'מחק',
+                showDenyButton: true,
+                denyButtonText: 'בטל פעולה',
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    dbContact.doc(id).delete()
+            
+                    console.log("del")
+                    window.location.reload();
+                }
+              })
 
-    
+        }
+
+        function hendel(email){
+            window.open('mailto:'+email);
+        }
     }
+
 
     return(
     <div className="p-3  bg-info">
