494199206d30336cafc6d2d115269127d2ee4211
asih94 <asafhager94@gmail.com>
2021-11-16 17:35:43 +0200

    Update Footer.js

--- app/src/component/Footer.js
+++ app/src/component/Footer.js
@@ -2,6 +2,7 @@ import {React} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../style/footer.css"
 import { Link } from 'react-router-dom';
+
 function Footer(){
 
     return(
@@ -9,19 +10,19 @@ function Footer(){
     
         <footer className="bg-dark text-center text-white"  dir="rtl">
             <div className="row">
-                <div className="col-12">
-                    <Link to="contact">
-                        <p className="text-center text-info"><b>ליצירת קשר עם העמותה</b></p>
-                    </Link>
-                    <Link to="Donations">
-                        <p className="text-center text-info"><b>לתרומות לעמותה</b></p>
-                    </Link>
-                    <Link to="Associations">
-                        <p className="text-center text-info"><b>עמותות נוספות</b></p>
-                    </Link>
-                    <Link to="Taxi">
-                        <p className="text-center text-info"><b> הזמנת מוניות</b></p>
-                    </Link>
+                <div className="col-lg-12">
+                        <Link to="contact">
+                            <p className="text-center text-info"><b>ליצירת קשר עם העמותה</b></p>
+                        </Link>
+                        <Link to="Donations">
+                            <p className="text-center text-info"><b>לתרומות לעמותה</b></p>
+                        </Link>
+                        <Link to="Associations">
+                            <p className="text-center text-info"><b>עמותות נוספות</b></p>
+                        </Link>
+                        <Link to="Taxi">
+                            <p className="text-center text-info"><b> הזמנת מוניות</b></p>
+                        </Link>
                 </div>
                 <div className="col-6"></div>
             </div>
